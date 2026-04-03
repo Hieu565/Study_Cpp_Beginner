339bd56eb92ced6d67cfb29754dd6502ce0e78aa
Hieu565 <67696469+Hieu565@users.noreply.github.com>
2026-04-03 17:09:09 +0700

    Create functions.hpp

---
+++ Memory_Pointer/Exercise_2/functions.hpp
@@ -0,0 +1,10 @@
+// declare variable
+class Letter  {
+public:
+  std::string word = "broccoli";
+  std::string text = "I eat broccoli, broccoli tastes good. There are three interesting things about broccoli. Number One. Nobody knows how to spell it. Number Two. No matter how long you boil it, it's always cold by the time it reaches your plate. Number Three. It's green";
+};
+
+// call functions
+void bleep(std::string word,std::string &text);
+void update(std::string word, std::string &text, int a);
