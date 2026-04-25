5b6f5adc3404943492a89b95a9580b0d82f75a95
Hieu565 <67696469+Hieu565@users.noreply.github.com>
2026-04-25 15:51:05 +0700

    Update profile.hpp

--- Class_and_Objects/Match_maker/profile.hpp
+++ Class_and_Objects/Match_maker/profile.hpp
@@ -2,6 +2,18 @@
 #include <iostream>
 #include <vector>
 
+class Input  {
+public:
+  // Input variable
+  std::string n_name;
+  int n_age;
+  std::string n_city;
+  std::string n_country;
+  std::string gender;
+  std::string n_pronouns;
+  std::string n_hobbies;
+};
+
 class Profile {
 
 // private cannot be accessed
@@ -11,11 +23,12 @@ private:
   std::string city;
   std::string country;
   std::string pronouns;
+  std::string gender;
   std::vector<std::string> hobbies;
 
 // public can be accessed anywhere
 public:
-  Profile(std::string new_name,int new_age,std::string new_city,std::string new_country,std::string new_pronouns = "they/them");
+  Profile(std::string new_name,int new_age,std::string new_city,std::string new_country,std::string new_gender,std::string new_pronouns = "they/them");
   std::string view_profile();
   void add_hobby(std::string new_hobby);
 
@@ -23,5 +36,5 @@ public:
 
 // functions of system
 void greet();
-void gender_reference(std::string gender,std::string pronouns);
-void ask(std::string &name, int &age, std::string &city, std::string &country);   // use & before variable to allow update
+void gender_reference(std::string gender, std::string &pronouns);
+void ask(std::string &name, int &age, std::string &city, std::string &country, std::string &gender, std::string &pronouns);   // use & before variable to allow update
